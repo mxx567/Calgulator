a0e91ec17ae38d0e7c9dc1c23c4576b5c622723c
mxx567 <themaks567@gmail.com>
2026-07-15 13:11:52 +0500

    Improved button layout

--- App.tsx
+++ App.tsx
@@ -4,6 +4,12 @@ import CircButton from './src/components/circButton';
 import { useState } from 'react';
 
 
+const buttons:string[][] = [
+    ['7', '8', '9', '/'],
+    ['4', '5', '6', '*'],
+    ['1', '2', '3', '-'],
+    ['.', '0', '=', '+']
+];
 
 
 export default function App() {
@@ -13,12 +19,15 @@ export default function App() {
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Text>{exp}</Text>
-      
-      <View style={styles.buttons}>
-        
-        <CircButton text='1' buttonSize={60} textSize = {30}onPress={() => setExp(exp + '1')}/>
-        
-      </View>
+      {
+        buttons.map((buttrow : string[], key: number) => 
+          <View style = {styles.buttonsRow} key = {key}>
+            {
+              buttrow.map((butt: string, key: number) => <CircButton text={butt} key={key} onPress={() => {butt != '=' ? setExp(exp + butt) : setExp(eval(exp))}}/>)
+            }
+          </View> 
+        )
+      }
       
     </View>
   );
@@ -31,15 +40,10 @@ const styles = StyleSheet.create({
     flex:1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center', 
+    justifyContent: 'space-evenly', 
     marginTop: 270,
   },
-  buttons:{
-    paddingBlock:420,
-    flexWrap: 'wrap',
-    justifyContent: 'space-evenly',
-    alignContent: 'flex-start',
-    gap: 5,
-    marginTop: -400,
+  buttonsRow:{
+    flexDirection:'row'
   }
 });
