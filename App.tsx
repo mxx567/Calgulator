c0e4ff788d076d67981f2e02075cdedce8295f09
mxx567 <themaks567@gmail.com>
2026-07-16 18:21:45 +0500

    Added checkSyntax function

--- App.tsx
+++ App.tsx
@@ -4,12 +4,34 @@ import CircButton from './src/components/circButton';
 import { useState } from 'react';
 
 
+
 const buttons:string[][] = [
     ['7', '8', '9', '/'],
     ['4', '5', '6', '*'],
     ['1', '2', '3', '-'],
     ['.', '0', '=', '+']
 ];
+
+
+function isIn(arr : any[], elem : any){
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] == elem){
+      return true;
+    }
+  }
+  return false;
+}
+
+const checkSyntax = (exp : string, symb : string) =>{
+  const sbs:string[] = ['/','*','-','+'];
+  if(isIn(sbs, exp.charAt(exp.length-1)) && isIn(sbs, symb)){
+      exp = exp.slice(0,exp.length-1) + symb;
+      console.log(exp)
+      return (exp);
+    }
+  return(exp + symb);
+}
+
 
 
 export default function App() {
@@ -23,7 +45,7 @@ export default function App() {
         buttons.map((buttrow : string[], key: number) => 
           <View style = {styles.buttonsRow} key = {key}>
             {
-              buttrow.map((butt: string, key: number) => <CircButton text={butt} key={key} onPress={() => {butt != '=' ? setExp(exp + butt) : setExp(eval(exp))}}/>)
+              buttrow.map((butt: string, key: number) => <CircButton text={butt} key={key} textSize={30} buttonSize={70} onPress={() => {butt != '=' ? setExp(checkSyntax(exp, butt)) : setExp(String(eval(exp)))}}/>)
             }
           </View> 
         )
@@ -44,6 +66,7 @@ const styles = StyleSheet.create({
     marginTop: 270,
   },
   buttonsRow:{
-    flexDirection:'row'
+    flexDirection:'row',
+    gap:15
   }
 });
